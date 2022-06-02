e68de53852ba13be27a3dfe5e50a2c3b1685543b
Sinval Junior <sinval.junior@sinch.com>
2022-06-02 00:13:47 -0300

    Making seeds independent

--- seed.js
+++ seed.js
@@ -3,11 +3,13 @@ const net = require("net");
 const axios = require("axios");
 
 const port = process.env.PORT;
-const folder = "_test_files/";
+const folder = process.env.FOLDER || "_test_files/";
+const seedAddress = `localhost:${port}`;
+const fileNames = fs.readdirSync(folder);
+
 const registerFiles = async () => {
   const indexAddress = "http://localhost:5001/register";
   const address = `localhost:${port}`;
-  const fileNames = fs.readdirSync(folder);
   let files = [];
 
   fileNames.map((name) => {
@@ -30,9 +32,30 @@ const registerFiles = async () => {
   );
 };
 
+const getFiles = async () => {
+  const indexAddress = "http://localhost:5001/files";
+  const response = await axios.get(indexAddress);
+  return response.data;
+};
+
 const main = async () => {
   await registerFiles();
-  
+
+  const files = await getFiles();
+
+  files
+  .filter(({ name }) => !fileNames.includes(name))
+  .map(({ address, name }) => {
+    const [host, port] = address.split(":");
+    const socket = net.createConnection({ port, host }, () => {});
+    socket.write(`${name};${seedAddress}`);
+    socket.on("data", (data) => {
+      fs.writeFileSync(`${folder}/${name}`, data);
+      socket.end();
+      console.log("done");
+    });
+  });
+
   const server = net.createServer((socket) => {
     socket.on("data", (data) => {
       const [fileName, peerAddress] = data.toString().split(";");
