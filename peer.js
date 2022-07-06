9da25cd03cce74f67a47c246c5bdbf157e568b49
gustavobritodam <gustavobritodam@gmail.com>
2022-07-06 11:38:38 -0300

    Tolerancia a falhas e log de falhas
    Implementação do hash para tolerancia a falhas
    
    Hasheei só o endereço(estilo switchs Cut-through), mas dá pra hashear o arquivo inteiro quando enviar os arquivos
    
    Tem que colocar a parte do log em mais locais
    
    Falta implementar algum tipo de autenticação para o nó que vai conectar com o server, pra não rolar injeção de dados nem ddos

--- peer.js
+++ peer.js
@@ -5,23 +5,22 @@ const crypto = require("crypto");
 
 const port = process.env.PORT;
 const folder = process.env.FOLDER || "_test_files/";
-const indexBaseAddress = "http://localhost:5001"
-const peerBaseAddress = "localhost"
+const indexBaseAddress = "http://localhost:5001";
+const peerBaseAddress = "localhost";
 const fileNames = fs.readdirSync(folder);
 
 const { publicKey, privateKey } = crypto.generateKeyPairSync("rsa", {
   modulusLength: 2048,
-  namedCurve: 'secp256k1',
+  namedCurve: "secp256k1",
   publicKeyEnconding: {
-    type: 'spki',
-    format: 'pem'
+    type: "spki",
+    format: "pem",
   },
   privateKeyEnconding: {
-    type: 'pkcs8',
-    format: 'der'
-  }
+    type: "pkcs8",
+    format: "der",
+  },
 });
-
 
 const registerFiles = async () => {
   const indexAddress = `${indexBaseAddress}/register`;
@@ -32,7 +31,13 @@ const registerFiles = async () => {
     files.push({
       name,
       address,
-      publicKey: publicKey.export({ format: 'pem', type: 'spki' }).toString('hex')
+      publicKey: publicKey
+        .export({ format: "pem", type: "spki" })
+        .toString("hex"),
+      hashDigest: crypto
+        .createHash("sha256")
+        .update(name, address)
+        .digest("hex"),
     });
   });
 
@@ -61,20 +66,31 @@ const main = async () => {
   const files = await getFiles();
 
   files
-    .filter(({ name }) => !fileNames.includes(name))
-    .map(({ address, name, publicKey: seedPublicKeyPEM }) => {
+    .filter(({ name }) => fileNames.includes(name))
+    .map(({ address, name, publicKey: seedPublicKeyPEM, hashDigest }) => {
+      hash = crypto.createHash("sha256").update(name, address).digest("hex");
+      if (hash != hashDigest) {
+        let timestamp = new Date().getTime();
+        console.log(`Something went wrong with ${name} file transfer from ${indexBaseAddress} to ${address} at ${timestamp}`);
+        return;
+      }
+
       const [host, port] = address.split(":");
-      const socket = net.createConnection({ port, host }, () => { });
+      const socket = net.createConnection({ port, host }, () => {});
       socket.write(name);
       socket.on("data", (encryptedData) => {
         const seedPublicKey = crypto.createPublicKey({
-          key: seedPublicKeyPEM.toString(), format: 'pem', type: 'spki'
+          key: seedPublicKeyPEM.toString(),
+          format: "pem",
+          type: "spki",
         });
 
         const fileContent = crypto.publicDecrypt(seedPublicKey, encryptedData);
         fs.writeFileSync(`${folder}/${name}`, fileContent);
         socket.end();
-        console.log(`Done! Downloaded ${name}`);
+
+        let timestamp = new Date().getTime();
+        console.log(`Done! ${address}received ${name} from ${indexBaseAddress} at ${timestamp}`);
       });
     });
 
